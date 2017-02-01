30d1ae50dad648e59085080c3bdc81444ef8ca7e
Onur BAŞTÜRK <obasturk@anadolu.edu.tr>
2017-02-01 15:40:19 +0300

    add new string manipulation algorithms

---
+++ src/main/java/com/basturk/stringmanipulation/RemoveDuplicateCharacters.java
@@ -0,0 +1,8 @@
+package main.java.com.basturk.stringmanipulation;
+
+/**
+ * Created by: onur
+ * Date: 2/1/17
+ */
+public class RemoveDuplicateCharacters {
+}
--- src/main/java/com/basturk/stringmanipulation/StringReverse.java
+++ src/main/java/com/basturk/stringmanipulation/StringReverse.java
@@ -2,6 +2,9 @@ package main.java.com.basturk.stringmanipulation;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by: onur
@@ -49,6 +52,31 @@ public class StringReverse {
 
         return reverseRecursively(str.substring(1)) + str.charAt(0);
 
+    }
+
+    /* * Method to reverse words in String in Java */
+    public static String reverseWords(String sentence) {
+        List<String> words = Arrays.asList(sentence.split("\\s"));
+        Collections.reverse(words);
+        StringBuilder sb = new StringBuilder(sentence.length());
+        for (int i = words.size() - 1; i >= 0; i--) {
+            sb.append(words.get(i));
+            sb.append(' ');
+        }
+        return sb.toString().trim();
+    }
+
+    public static String reverseString(String line) {
+        if (line.trim().isEmpty()) {
+            return line;
+        }
+        StringBuilder reverse = new StringBuilder();
+        String[] sa = line.trim().split("\\s");
+        for (int i = sa.length - 1; i >= 0; i--) {
+            reverse.append(sa[i]);
+            reverse.append(' ');
+        }
+        return reverse.toString().trim();
     }
 
 
